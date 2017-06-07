03709e2860e6a4f3e4ef63cd8541d4ffedf0053a
Frode Sjovatsen <frode@sjovatsen.net>
2017-06-07 18:27:14 +0200

    added linkmapper config

---
+++ src/main/java/no/fint/consumer/Config.java
@@ -0,0 +1,34 @@
+package no.fint.consumer;
+
+import com.google.common.collect.ImmutableMap;
+import no.fint.consumer.utils.RestEndpoints;
+import no.fint.model.administrasjon.organisasjon.Organisasjonselement;
+import no.fint.model.administrasjon.personal.Arbeidsforhold;
+import no.fint.model.administrasjon.personal.Personalressurs;
+import no.fint.model.felles.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class Config {
+
+    @Value("${server.contextPath:}")
+    private String contextPath;
+
+    @Qualifier("linkMapper")
+    @Bean
+    public Map<String, String> linkMapper() {
+        return ImmutableMap.of(
+                Organisasjonselement.class.getName(), "/administrasjon/organisasjon/organisasjonselement"
+        );
+    }
+
+    private String fullPath(String path) {
+        return String.format("%s%s", contextPath, path);
+    }
+
+}
