81b2d8159897ddc0e10537698c3061b6ab6f9cf3
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-11-21 16:19:56 +0100

    Version 1.2.0
    Updated to version 2.2.0 of information model.
    Added GET mapping by organisasjonsnummer.

--- src/main/java/no/fint/consumer/config/ConsumerProps.java
+++ src/main/java/no/fint/consumer/config/ConsumerProps.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class ConsumerProps {
 
     public static final String CACHE_INITIALDELAY_ORGANISASJONSELEMENT = "${fint.consumer.cache.initialDelay.organisasjonselement:40000}";
-    public static final String CACHE_FIXEDRATE_ORGANISASJONSELEMENT = "${fint.consumer.cache.fixedRate.organisasjonselement:55000}";
+    public static final String CACHE_FIXEDRATE_ORGANISASJONSELEMENT = "${fint.consumer.cache.fixedRate.organisasjonselement:900000}";
 
     @Value("${fint.events.orgIds:mock.no}")
     private String[] orgs;
