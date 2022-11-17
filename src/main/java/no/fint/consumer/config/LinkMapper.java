5ff8064eeca8f6fa01a89b1d92ff791606d1affb
Nils-Odd Solberg <nilsodd@fintlabs.no>
2022-11-17 11:57:27 +0100

    Version 3.12.0.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -3,18 +3,20 @@ package no.fint.consumer.config;
 import no.fint.consumer.utils.RestEndpoints;
 import java.util.Map;
 import com.google.common.collect.ImmutableMap;
+import no.fint.model.administrasjon.organisasjon.Arbeidslokasjon;
 import no.fint.model.administrasjon.organisasjon.Organisasjonselement;
 
 public class LinkMapper {
 
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String,String>builder()
+            .put(Arbeidslokasjon.class.getName(), contextPath + RestEndpoints.ARBEIDSLOKASJON)
             .put(Organisasjonselement.class.getName(), contextPath + RestEndpoints.ORGANISASJONSELEMENT)
             .put("no.fint.model.felles.kodeverk.iso.Landkode", "/felles/kodeverk/iso/landkode")
+            .put("no.fint.model.administrasjon.personal.Arbeidsforhold", "/administrasjon/personal/arbeidsforhold")
             .put("no.fint.model.administrasjon.kodeverk.Ansvar", "/administrasjon/kodeverk/ansvar")
             .put("no.fint.model.administrasjon.personal.Personalressurs", "/administrasjon/personal/personalressurs")
             .put("no.fint.model.utdanning.utdanningsprogram.Skole", "/utdanning/utdanningsprogram/skole")
-            .put("no.fint.model.administrasjon.personal.Arbeidsforhold", "/administrasjon/personal/arbeidsforhold")
             /* .put(TODO,TODO) */
             .build();
     }
--- src/main/java/no/fint/consumer/utils/RestEndpoints.java
+++ src/main/java/no/fint/consumer/utils/RestEndpoints.java
@@ -4,6 +4,7 @@ public enum RestEndpoints {
     ;
 
     public static final String ADMIN = "/admin";
+	public static final String ARBEIDSLOKASJON = "/arbeidslokasjon";
 	public static final String ORGANISASJONSELEMENT = "/organisasjonselement";
 
 }
