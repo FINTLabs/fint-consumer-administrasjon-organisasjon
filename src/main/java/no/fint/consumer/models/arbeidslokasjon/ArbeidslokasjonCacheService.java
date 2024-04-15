b8b34812ac533a345a7da39ec47b1c7b3216b99f
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2024-04-15 10:40:51 +0200

    new fint-releations (#27)

--- src/main/java/no/fint/consumer/models/arbeidslokasjon/ArbeidslokasjonCacheService.java
+++ src/main/java/no/fint/consumer/models/arbeidslokasjon/ArbeidslokasjonCacheService.java
@@ -117,7 +117,10 @@ public class ArbeidslokasjonCacheService extends CacheService<ArbeidslokasjonRes
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (OrganisasjonActions.valueOf(event.getAction()) == OrganisasjonActions.UPDATE_ARBEIDSLOKASJON) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<ArbeidslokasjonResource>> cacheObjects = data
